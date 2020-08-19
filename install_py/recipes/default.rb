dbc89d4761cf2865546454260195b1ac7edac9cf
Blamas <blamas@estantevirtual.com.br>
2020-08-19 13:06:48 -0300

    ajustando diretórios

--- install_py/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-#Instalando pacote python-pip
-execute 'upgrade' do
-    command "pip install --upgrade pip"
-end
-
-execute 'upgrad virtualenv' do
-    command "pip install --upgrade virtualenv --ignore-installed six"
-end
--- recipes/python.rb
+++
@@ -1,4 +0,0 @@
-#Instalando pacote python-pip
-execute 'upgrade' do
-    command "pip install --upgrade pip"
-end
