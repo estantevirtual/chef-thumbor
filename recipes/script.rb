d6c512d04898737e8b2e83f9eb2d83ecd12a977a
Blamas <blamas@estantevirtual.com.br>
2020-08-24 12:22:58 -0300

    remove alterações

--- recipes/script.rb
+++
@@ -1,9 +0,0 @@
-script "upgrade_python" do
-    interpreter "bash"
-    user "root"
-    cwd "/tmp"
-    code <<-EOH
-      pip install --upgrade pip
-      pip install --upgrade virtualenv --ignore-installed six
-    EOH
-end
