18b0358f3fb8b1a24f5074f1df624b0f511c266d
Blamas <blamas@estantevirtual.com.br>
2020-08-24 12:34:30 -0300

    Atualiza os recursos necessários para o thumbor

---
+++ recipes/pipupgrade.rb
@@ -0,0 +1,17 @@
+#Atualiza os recursos necessários para o thumbor
+
+execute 'upgrad virtualenv' do
+    command "pip install --upgrade virtualenv --ignore-installed six"
+end
+
+execute 'install tornado' do
+    command "sudo pip install 'Tornado>=4.0.0,<5.0.0'"
+end
+
+execute 'libgraphicsmagick++1-dev' do
+    command "sudo apt-get install libgraphicsmagick++1-dev libboost-python-dev"
+end
+
+execute 'graphicsmagick-engine' do
+    command "pip install graphicsmagick-engine"
+end
